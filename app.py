a4582d156a100cc2f71b83c17bbede3f71576581
guggiaosca <oscar.guggiana@neocol.com>
2022-02-17 21:51:53 +0000

    debug launch.json and new app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from forex_python.converter import CurrencyRates
 from flask import Flask, request, jsonify
+import datetime
 
 app = Flask(__name__)
 c = CurrencyRates()
@@ -10,4 +11,16 @@ def get_rates():
 
 @app.get("/rates/<code>")
 def get_ratesById(code):
+    args = request.args;
+    exchange_date = args.get("exchange_date")
+    convert_to = args.get("currency")
+    if None not in (exchange_date, convert_to):
+        date_time_obj = datetime.datetime.strptime(exchange_date, '%Y-%m-%d')
+        return str(c.get_rate(code, convert_to, date_time_obj))
+    elif exchange_date is not None:
+        date_time_obj = datetime.datetime.strptime(exchange_date, '%Y-%m-%d')
+        return str(c.get_rate(code, date_time_obj))
+    elif convert_to is not None:
+        retorno = str(c.get_rate(code, convert_to))
+        return retorno
     return c.get_rates(code)
